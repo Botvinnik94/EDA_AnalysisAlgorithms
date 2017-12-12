7d120e535605e071e782ad1dbff52beefd870558
Gabino Luis <gabinoluis@hotmail.es>
2017-12-12 10:30:11 +0100

    SinglyLinkedLists util library added

--- utils/list/singleLinkedList/singleLinkedList.c
+++ utils/list/singleLinkedList/singleLinkedList.c
@@ -1,11 +1,11 @@
 #include "singleLinkedList.h"
 
-node_t* SList_new_node(){
+SNode_t* SList_new_node(){
 
-    node_t *Node;
+    SNode_t *Node;
 
-    Node = malloc(sizeof(node_t));
-    return_if_null(Node);
+    Node = malloc(sizeof(SNode_t));
+    return_null_if(Node == NULL)
 
     return Node;
 }
@@ -15,13 +15,10 @@ SList_t* SList_new_singleLinkedList() {
     SList_t *List;
 
     List = malloc(sizeof(SList_t));
-    return_if_null(List);
+    return_null_if(List == NULL)
 
-    List->first = new_node();
-    if(List->first == NULL){
-        free(List);
-        return NULL;
-    }
+    List->first = SList_new_node();
+	return_null_and_free(List->first == NULL, List)
 
     List->last = List->first;
 
@@ -29,25 +26,26 @@ SList_t* SList_new_singleLinkedList() {
 }
 
 SList_t* SList_generateInRange(size_t size, type_t min, type_t max){
-    SList_t* List = new_singleLinkedList();
-    return_if_null(List);
+    SList_t* List = SList_new_singleLinkedList();
+    return_null_if(List == NULL)
 
     size_t i;
 
     srand(time(NULL));
     for(i = 0; i < size; i++){
         type_t val = min + (rand() % (max - min));
-        insert(val, List->last, List);
+        SList_insert(val, List->last, List);
     }
 
     return List;
 }
 
-void SList_insert(type_t val, node_t* pos, SList_t* list){
+void SList_insert(type_t val, SNode_t* pos, SList_t* list){
 
-    //TODO: error and nullPointer checking
+	return_if(list == NULL)
 
-    node_t* newNode = new_node();
+    SNode_t* newNode = SList_new_node();
+	return_if(newNode == NULL)
 
     newNode->info = val;
     newNode->next = pos->next;
@@ -56,37 +54,66 @@ void SList_insert(type_t val, node_t* pos, SList_t* list){
     if(pos == list->last) list->last = newNode;
 }
 
-node_t* SList_seek(type_t val, SList_t* list){
+SNode_t* SList_seek(type_t val, SList_t* list){
 
-    return_if_null(list);
-    node_t* iterator = list->first;
+    return_null_if(list == NULL)
+    SNode_t* iterator = list->first;
     
     while(iterator->next != NULL){
-        iterator = iterator->next;
-        if(iterator->info == val) return iterator;
+        if(iterator->next->info == val) return iterator;
+		iterator = iterator->next;
     }
 
-    return NULL;
+    return iterator;
 }
 
-void SList_remove(node_t* pos, SList_t* list){
+void SList_remove(SNode_t* pos, SList_t* list){
 
-    //TODO: null pointer exception checking
+    return_if(list == NULL)
 
-    node_t* toRemove = pos->next;
+    SNode_t* toRemove = pos->next;
     pos->next = toRemove->next;
+
+	if(toRemove == list->last)
+		list->last = pos;
+
     free(toRemove);
 
-    //TODO: update last if u kill the last node
 }
 
 void SList_clear(SList_t* list){
 
-    //TODO: null pointer exception checking
-    node_t* pos = list->first;
+    return_if(list == NULL)
+    SIterator_t it = list->first;
 
-    while(pos->next != NULL){
-        SList_remove(pos, list);
-        pos = pos->next;
+    while(it->next != NULL){
+        SList_remove(it, list);        
     }
+}
+
+SNode_t* SList_previous(SNode_t* pos, SList_t* list){
+
+	return_null_if(list == NULL)
+	SIterator_t it = list->first;
+
+	while(it->next != NULL){
+		if(it->next == pos) return it;
+		it = it->next;
+	}
+	
+	return it;
+}
+
+
+//Not a generic function, only works with type_t as integers
+void SList_print(SList_t* list){
+
+	return_if(list == NULL);
+	SIterator_t it = list->first;
+
+	while(it->next != NULL){
+		printf("%d\t", it->next->info);
+		it = it->next;
+	}
+	printf("\n");
 }
--- utils/list/singleLinkedList/singleLinkedList.h
+++ utils/list/singleLinkedList/singleLinkedList.h
@@ -1,35 +1,61 @@
 #ifndef __SINGLE_LINKED_LIST_H
 #define __SINGLE_LINKED_LIST_H
 
-#include "../../../tester/AlgorithmTester.h"
+#include<stdio.h>
+#include<stdlib.h>
+#include<time.h>
 
-#define return_if_null(exp)     if((exp) == NULL) return NULL;
+#define return_if(expr)						if(expr) return;
+#define return_val_if(expr, val)			if(expr) return val;
+#define return_null_if(expr)     			return_val_if(expr, NULL)
+#define return_null_and_free(expr, toFree)								\
+	if(expr){															\
+		free(toFree);													\
+		return NULL;													\
+	}	
+		
 
 typedef int type_t;
-typedef struct node node_t;
+typedef struct node SNode_t;
 
 struct node{
     type_t info;
-    node_t* next; 
+    SNode_t* next; 
 };
 
 typedef struct slList {
-    node_t* first;
-    node_t* last;
+    SNode_t* first;
+    SNode_t* last;
 } SList_t;
+
+typedef SNode_t* SIterator_t;
 
 SList_t* SList_generateInRange(size_t size, type_t min, type_t max);
 
 SList_t* SList_new_singleLinkedList();
 
-node_t* SList_new_node();
+SNode_t* SList_new_node();
 
-void SList_insert(type_t val, node_t* pos, SList_t* list);
+void SList_insert(type_t val, SNode_t* pos, SList_t* list);
 
-node_t* SList_seek(type_t val, SList_t* list);
+SNode_t* SList_seek(type_t val, SList_t* list);
 
-void SList_remove(node_t* pos, SList_t* list);
+void SList_remove(SNode_t* pos, SList_t* list);
 
 void SList_clear(SList_t* list);
+
+SNode_t* SList_previous(SNode_t* pos, SList_t* list);
+
+#define SList_get(pos)			pos->next->info
+
+#define SList_first(list)		list->first
+
+#define SList_last(list)		list->last
+
+#define SList_next(pos)			pos->next
+
+#define SList_add(val, list)	SList_insert(val, list->last, list)
+
+void SList_print(SList_t* list);	
 
 #endif
